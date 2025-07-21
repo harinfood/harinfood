6085871072a319d0a589d7a62b0b7216098b270b
harinfood <rhininyahari1@gmail.com>
2025-07-21 21:38:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -258,8 +258,8 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 3, nama: "Citung", harga: 2500, gambar: "citung.webp", barcode: "citung" },
         { id: 4, nama: "Tteokbokki", harga: 5000, gambar: "toppoki.webp", barcode: "toppoki" },
         { id: 5, nama: "Tteokbokki", harga: 10000, gambar: "toppoki.webp", barcode: "toppoki10" },
-        { id: 6, nama: "spaghetti", harga: 6000, gambar: "spaghetti.webp", barcode: "spaghetti" },
-        { id: 7, nama: "spaghetti", harga: 10000, gambar: "spaghetti1.webp", barcode: "spaghetti1" },
+        { id: 6, nama: "spaghetti tanpa toping", harga: 6000, gambar: "spaghetti.webp", barcode: "spaghetti" },
+        { id: 7, nama: "spaghetti dengan toping", harga: 10000, gambar: "spaghetti1.webp", barcode: "spaghetti1" },
         { id: 9, nama: "Es Teh Jumbo", harga: 3000, gambar: "esteh.webp", barcode: "esteh" },
         { id: 10, nama: "Es Teh kecil", harga: 2000, gambar: "esteh.webp", barcode: "esteh2" }
     ];
