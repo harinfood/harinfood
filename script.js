437318a4c859a0418c25760ce1f2e11796a690dd
harinfood <rhininyahari1@gmail.com>
2025-07-26 09:44:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -269,7 +269,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 5, nama: "Tteokbokki", harga: 10000, gambar: "toppoki.webp", barcode: "toppoki10" },
         { id: 6, nama: "spaghetti tanpa toping", harga: 6000, gambar: "spaghetti.webp", barcode: "spaghetti" },
         { id: 7, nama: "spaghetti dengan toping", harga: 10000, gambar: "spaghetti1.webp", barcode: "spaghetti1" },
-        { id: 8, nama: "Balungan", harga: 10000, gambar: "balungan.webp", barcode: "balungan" },
+        { id: 8, nama: "Balungan", harga: 5000, gambar: "balungan.webp", barcode: "balungan" },
         { id: 9, nama: "Es Teh Jumbo", harga: 3000, gambar: "esteh.webp", barcode: "esteh" },
         { id: 10, nama: "Es Teh kecil", harga: 2000, gambar: "esteh1.webp", barcode: "esteh2" }
     ];
