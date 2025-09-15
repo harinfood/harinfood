ad431db457ae39767d238812c65495562decb42b
harinfood <rhininyahari1@gmail.com>
2025-09-16 03:40:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // sw.js - Service Worker HARINFOOD POS Lite (Optimal for Automatic Updates & Offline)
 // GANTI versi cache SETIAP kali ada update file!
-const CACHE_VERSION = 'v43'; // Ganti setiap update!
+const CACHE_VERSION = 'v44'; // Ganti setiap update!
 const CACHE_NAME = `harinfood-cache-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/style.css?v=43',
-  '/script.js?v=43',
-  '/manifest.json?v=43',
+  '/style.css?v=44',
+  '/script.js?v=44',
+  '/manifest.json?v=44',
   // Tambahkan semua gambar/audio/icon di bawah ini ok
   '/risol.webp',
   '/cibay.webp',
