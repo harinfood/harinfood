bad56dd21207c29f74b5ed7e46c1df04bcf09bf6
harinfood <rhininyahari1@gmail.com>
2026-04-13 07:43:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -354,7 +354,7 @@ if (!cekJamOperasional()) {
     }
     createKembalianModal();
     const produkData = [
-        { id: 1, nama: "Risol", harga: 3000, gambar: "risol.webp", barcode: "risol", stok: 1, kategori:"makanan"},
+        { id: 1, nama: "Risol ayam & sayuran", harga: 3000, gambar: "risol.webp", barcode: "risol", stok: 1, kategori:"makanan"},
         { id: 20, nama: "Risol saos pizza & keju", harga: 3000, gambar: "risolpizza.webp", barcode: "risol1", stok: 1, kategori:"makanan"},  
         { id: 2, nama: "Cibay", harga: 2500, gambar: "cibay.webp", barcode: "cibay" , stok: 1, kategori:"makanan"},
        { id: 4, nama: "Tteokbokki 5K", harga: 5000, gambar: "toppoki.webp", barcode: "toppoki", stok: 1, kategori:"makanan"},
--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // sw.js - Service Worker HARINFOOD POS Lite (Optimal for Automatic Updates & Offline)
 // GANTI versi cache SETIAP kali ada update file!
-const CACHE_VERSION = 'v154-header-kategori'; // Ganti setiap update!
+const CACHE_VERSION = 'v155-header-kategori'; // Ganti setiap update!
 const CACHE_NAME = `harinfood-cache-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html?v=154',
-  '/style.css?v=154',
-  '/script.js?v=154',
-  '/manifest.json?v=154',
+  '/index.html?v=155',
+  '/style.css?v=155',
+  '/script.js?v=155',
+  '/manifest.json?v=155',
   // Tambahkan semua gambar/audio/icon di bawah ini ok
   '/risol.webp',
   '/cibay.webp',
