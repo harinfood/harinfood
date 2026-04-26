c07ff0bb26dd24e4f523288cead4f0230494d6e5
harinfood <rhininyahari1@gmail.com>
2026-04-26 09:46:14 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // sw.js - Service Worker HARINFOOD POS Lite (Optimal for Automatic Updates & Offline)
 // GANTI versi cache SETIAP kali ada update file!
-const CACHE_VERSION = 'v166-header-kategori'; // Ganti setiap update!
+const CACHE_VERSION = 'v167-header-kategori'; // Ganti setiap update!
 const CACHE_NAME = `harinfood-cache-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html?v=166',
-  '/style.css?v=166',
-  '/script.js?v=166',
-  '/manifest.json?v=166',
+  '/index.html?v=167',
+  '/style.css?v=167',
+  '/script.js?v=167',
+  '/manifest.json?v=167',
   // Tambahkan semua gambar/audio/icon di bawah ini ok
   '/risol.webp',
   '/cibay.webp',
