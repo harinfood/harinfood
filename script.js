bb5945dcecc01c858ef5e82cad8b8946ce37d1ae
harinfood <rhininyahari1@gmail.com>
2026-04-27 10:15:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -357,11 +357,12 @@ if (!cekJamOperasional()) {
     createKembalianModal();
     const produkData = [
         { id: 1, nama: "Risol ayam & sayuran", harga: 3000, gambar: "risol.webp", barcode: "risol",stok: 1, kategori:"makanan"},
-        { id: 2, nama: "Cibay", harga: 2500, gambar: "cibay.webp", barcode: "cibay" , stok: 1, kategori:"makanan"},
+       { id: 20, nama: "Risol saos Bolognese & keju", harga: 3000, gambar: "risolpizza.webp", barcode: "risol1", stok: 1, kategori:"makanan"},          
+      { id: 2, nama: "Cibay", harga: 2500, gambar: "cibay.webp", barcode: "cibay" , stok: 1, kategori:"makanan"},
        { id: 4, nama: "Tteokbokki 5K", harga: 5000, gambar: "toppoki.webp", barcode: "toppoki", stok: 1, kategori:"makanan"},
         { id: 5, nama: "Tteokbokki", harga: 10000, gambar: "toppoki1.webp", barcode: "toppoki10" , stok: 1, kategori:"makanan"},          
        { id: 7, nama: "spaghetti", harga: 10000, gambar: "spaghetti1.webp", barcode: "spaghetti1", stok: 0, kategori:"makanan"},
-      { id: 20, nama: "Risol saos Bolognese & keju", harga: 3000, gambar: "risolpizza.webp", barcode: "risol1", stok: 0, kategori:"makanan"},       
+ 
       // { id: 6, nama: "spaghetti tanpa toping", harga: 8000, gambar: "spaghetti.webp", barcode: "spaghetti", stok: 0 , kategori:"makanan"},      
         { id: 21, nama: "sosis bakar", harga: 5000, gambar: "sosis.webp", barcode: "sosis", stok: 0, kategori:"makanan"},
         { id: 21, nama: "sosis kecil bakar", harga: 2000, gambar: "sosis1.webp", barcode: "sosis1", stok: 0, kategori:"makanan"},             
--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // sw.js - Service Worker HARINFOOD POS Lite (Optimal for Automatic Updates & Offline)
 // GANTI versi cache SETIAP kali ada update file!
-const CACHE_VERSION = 'v167-header-kategori'; // Ganti setiap update!
+const CACHE_VERSION = 'v168-header-kategori'; // Ganti setiap update!
 const CACHE_NAME = `harinfood-cache-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html?v=167',
-  '/style.css?v=167',
-  '/script.js?v=167',
-  '/manifest.json?v=167',
+  '/index.html?v=168',
+  '/style.css?v=168',
+  '/script.js?v=168',
+  '/manifest.json?v=168',
   // Tambahkan semua gambar/audio/icon di bawah ini ok
   '/risol.webp',
   '/cibay.webp',
